a9f44ae3c9805c6b31e84480671a2266a4dc0b5e
mainul098 <mainul098@gmail.com>
2014-09-22 21:55:29 +0600

    Add user edit

--- src/web/js/tenant/controllers/tenant-controller.js
+++ src/web/js/tenant/controllers/tenant-controller.js
@@ -1,10 +1,6 @@
 define(['tenant/tenant-module'], function (tenantModule) {
     'use strict';
     tenantModule.controller('TenantController', ['$scope','$state', function ($scope, $state) {
-        $scope.createNewTenant = function(){
-            $state.go('tenant.create');
-        };
-
         $scope.downloadTenantCSVFile = function(){
             console.log('Download CSV file');
         };
--- src/web/js/user/controllers/user-edit-controller.js
+++ src/web/js/user/controllers/user-edit-controller.js
@@ -1,6 +1,13 @@
-define(['user/user-module'], function (userModule) {
+define(['user/user-module', 'userServices/user-service'], function (userModule) {
     'use strict';
-    userModule.controller('UserEditController', ['$scope', function ($scope) {
-        console.log('User Edit Controller running........');
+    userModule.controller('UserEditController', ['$scope','$stateParams', 'UserService', function ($scope, $stateParams, UserService) {
+        $scope.user ={};
+        $scope.userId = $stateParams.id;
+        (function(){
+            UserService.get({id:$scope.userId}, function(user){
+                console.log(user);
+                $scope.user = user;
+            });
+        })();
     }]);
 });
