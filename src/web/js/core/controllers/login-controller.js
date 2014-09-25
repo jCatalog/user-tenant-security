d42b23519ec6d41702c7a901705206775b89503b
mainul098 <mainul098@gmail.com>
2014-09-25 08:34:09 +0600

    Add Angular cookies

--- src/web/js/core/controllers/login-controller.js
+++ src/web/js/core/controllers/login-controller.js
@@ -1,9 +1,10 @@
 define(['core/core-module', 'coreServices/auth-service'], function (coreModule) {
     'use strict';
-    coreModule.controller('LoginController', ['$scope', '$state', '$cookieStore', 'AuthService', function ($scope, $state, $cookieStore, AuthService) {
+    coreModule.controller('LoginController', ['$scope', '$rootScope', '$state', '$cookieStore', 'AuthService', function ($scope, $rootScope, $state, $cookieStore, AuthService) {
         $scope.login = function (credentials) {
             AuthService.login(credentials).then(function (user) {
                 $cookieStore.put('UserTenantSecurityUserModel', user);
+                $scope.userModel = user;
                 $state.go('home');
             }, function () {
                 console.log('Error');
