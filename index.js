b0fb4386a663558e23e335c0144f26067dc90528
mainul098 <mainul098@gmail.com>
2014-10-12 19:38:58 +0600

    Remove index file

--- index.js
+++
@@ -1,7 +0,0 @@
-var Hapi = require('hapi');
-var manifest = require('./manifest');
-var composeOptions = {
-    relativeTo: __dirname
-};
-
-module.exports = Hapi.Pack.compose.bind(Hapi.Pack, manifest.get('/'), composeOptions);
