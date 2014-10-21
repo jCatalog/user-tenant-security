3c087981326bfbc464a872ced5c9b56ce6471ba8
mainul098 <mainul098@gmail.com>
2014-10-21 13:45:59 +0600

    Refactor the util

--- src/api/util/hapi-acl/index.js
+++ src/api/util/hapi-acl/index.js
@@ -5,13 +5,12 @@
  * @type {exports}
  */
 var acl = require('./lib/acl');
-var mongodbBackend = require('./lib/mongodb-backend');
+var backend = require('./lib/backend');
 /**
  * Export hapi-acl module
  * @param db
  * @returns {acl}
  */
 module.exports = function (db) {
-    var Acl = new acl(new mongodbBackend(db, 'hapi_acl_'));
-    return Acl;
+    return new acl(new backend(db, 'hapi_acl_'));
 };
